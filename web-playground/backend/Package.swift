78ea2de5f2ce0f13976bc5f5b3b0a7c0d31abcd1
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-05-25 13:32:47 +0200

    adjust backend package

--- web-playground/backend/Package.swift
+++ web-playground/backend/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(path: "../../swift-bedrock-library"),
+        .package(name: "swift-bedrock-library", path: "../.."),
     ],
     targets: [
         .executableTarget(
