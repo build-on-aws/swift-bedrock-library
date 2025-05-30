09bcdab1abf9f3a0dde0599d0ad34c379595b987
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-05-30 08:42:15 +0200

    change project name in license headers

--- Sources/BedrockService/Models/Mistral/MistralBedrockModels.swift
+++ Sources/BedrockService/Models/Mistral/MistralBedrockModels.swift
@@ -1,13 +1,13 @@
 //===----------------------------------------------------------------------===//
 //
-// This source file is part of the Swift Foundation Models Playground open source project
+// This source file is part of the Swift Bedrock Library open source project
 //
 // Copyright (c) 2025 Amazon.com, Inc. or its affiliates
-//                    and the Swift Foundation Models Playground project authors
+//                    and the Swift Bedrock Library project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of Swift Foundation Models Playground project authors
+// See CONTRIBUTORS.txt for the list of Swift Bedrock Library project authors
 //
 // SPDX-License-Identifier: Apache-2.0
 //
