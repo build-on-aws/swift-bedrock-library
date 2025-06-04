86ed3d66a479da9792b1a37f5bff44b72a7224ca
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-06-05 00:25:05 +0200

    fix license header

--- Sources/Converse/Streaming/ToolUseStart.swift
+++ Sources/Converse/Streaming/ToolUseStart.swift
@@ -1,17 +1,17 @@
-// //===----------------------------------------------------------------------===//
-// //
-// // This source file is part of the Swift Bedrock Library open source project
-// //
-// // Copyright (c) 2025 Amazon.com, Inc. or its affiliates
-// //                    and the Swift Bedrock Library project authors
-// // Licensed under Apache License v2.0
-// //
-// // See LICENSE.txt for license information
-// // See CONTRIBUTORS.txt for the list of Swift Bedrock Library project authors
-// //
-// // SPDX-License-Identifier: Apache-2.0
-// //
-// //===----------------------------------------------------------------------===//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Bedrock Library open source project
+//
+// Copyright (c) 2025 Amazon.com, Inc. or its affiliates
+//                    and the Swift Bedrock Library project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Bedrock Library project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 @preconcurrency import AWSBedrockRuntime
 
