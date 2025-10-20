d9d7b59045ed0faf80bb2264278c13a13afbd948
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-10-20 22:53:35 +0200

    remove account number

--- Examples/ios-math-solver/Sources/changeme.swift
+++ Examples/ios-math-solver/Sources/changeme.swift
@@ -16,5 +16,5 @@
 // to integrate with Sign In With Apple (SIWA), you must prepare your AWS account
 // Follow instructions at https://docs.aws.amazon.com/sdk-for-swift/latest/developer-guide/apple-integration.html#apple-sign-in
 
-let awsAccountNumber = "486652066693"  // TODO: Replace with your AWS account number
+let awsAccountNumber = "000000000000"  // TODO: Replace with your AWS account number
 let awsIAMRoleName = "ios-swift-bedrock"  // TODO: Replace with your IAM role name
