40da2e186d0f754f23904b68a54c1573017d6892
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-10-22 13:32:15 +0200

    fix license

--- Examples/embeddings/Sources/Embeddings.swift
+++ Examples/embeddings/Sources/Embeddings.swift
@@ -1,5 +1,17 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Bedrock Library open source project
+//
+// Copyright (c) 2025 Amazon.com, Inc. or its affiliates
+//                    and the Swift Bedrock Library project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Bedrock Library project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import BedrockService
 import Foundation
